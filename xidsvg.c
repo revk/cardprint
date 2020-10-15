b5540a0c5ac54d2680a8cfda70060e6af5989bbb
Adrian Kennard <a@k.gg>
2020-10-15 07:13:24 +0100

    More move to library

--- xidsvg.c
+++ xidsvg.c
@@ -30,7 +30,9 @@
 #include <execinfo.h>
 #include <axl.h>
 #include <ajl.h>
+#include "xidsvg.h"
 
+#ifndef LIB
 #define xquoted(x)      #x
 #define quoted(x)       xquoted(x)
 const char xidport[] = "7810";
@@ -64,6 +66,7 @@ int copies = 1;
 const char *input = NULL;
 const char *output = NULL;
 char *jsstatus = NULL;
+#endif
 
 ssize_t ss_write_func(void *arg, void *buf, size_t len)
 {
@@ -75,7 +78,146 @@ ssize_t ss_read_func(void *arg, void *buf, size_t len)
    return SSL_read(arg, buf, len);
 }
 
+j_t xid_compose(xml_t svg, int dpi, int rows, int cols, xid_status_t status)
+{                               // Convert SVG to png
+   status("Compose");
+   if (dpi < 0)
+      dpi = 300;
+   if (rows < 0)
+      rows = 664 * dpi / 300;
+   if (cols < 0)
+      cols = 1036 * dpi / 300;
+   char *tmpsvg = strdup("/tmp/cardXXXXXX.svg");
+   {
+      int f = mkstemps(tmpsvg, 4);
+      if (f < 0)
+         status("*Cannot make svg tmp file");
+      FILE *o = fdopen(f, "w");
+      xml_write(o, svg);
+      fclose(o);
+   }
+   const char layertag[] = "CKU";
+   char *tmp[2][3] = { };
+   pid_t pid[2][3] = { };
+   for (int side = 0; side < 2; side++)
+      for (int layer = 0; layer < 3; layer++)
+      {
+         xml_t find(xml_t x) {
+            const char *v = xml_get(x, "@id");
+            if (v && v[0] == layertag[layer] && v[1] == '1' + side && !v[2])
+               return x;
+            xml_t e = NULL,
+                q;
+            while ((e = xml_element_next(x, e)))
+               if ((q = find(e)))
+                  return q;
+            return NULL;
+         }
+         if (!find(svg))
+            continue;
+         tmp[side][layer] = strdup("/tmp/cardXX-XXXXXX.png");
+         tmp[side][layer][9] = layertag[layer];
+         tmp[side][layer][10] = '1' + side;
+         int f = mkstemps(tmp[side][layer], 4);
+         if (f < 0)
+            status("*Cannot make PNG tmp file");
+         close(f);
+         char id[3] = { layertag[layer], '1' + side };
+         if (!(pid[side][layer] = fork()))
+         {
+            char *args[100];
+            int a = 0;
+            args[a++] = "inkscape";
+            args[a++] = "--without-gui";
+            args[a++] = "--export-area-page";
+            args[a++] = "--export-id-only";
+            if (asprintf(&args[a++], "--export-png=%s", tmp[side][layer]) < 0)
+               status("*malloc");
+            if (asprintf(&args[a++], "--export-dpi=%d", dpi) < 0)
+               status("*malloc");
+            if (asprintf(&args[a++], "--export-id=%s", id) < 0)
+               status("*malloc");
+            args[a++] = tmpsvg;
+            args[a++] = NULL;
+            int n = open("/dev/null", 0);
+            dup2(n, 1);
+            dup2(n, 2);
+            close(n);
+            execv("/usr/bin/inkscape", (char *const *) args);
+            err(1, "Failed to run inkscape");
+         }
+      }
+   for (int side = 0; side < 2; side++)
+      for (int layer = 0; layer < 3; layer++)
+         if (tmp[side][layer])
+         {
+            int pstatus = 0;
+            waitpid(pid[side][layer], &pstatus, 0);
+            if (!WIFEXITED(pstatus) || WEXITSTATUS(pstatus))
+               status("*SVG conversion fail");
+         }
+   j_t j = j_create();
+   char *mag1 = xml_get(svg, "@track1");
+   char *mag2 = xml_get(svg, "@track2");
+   char *mag3 = xml_get(svg, "@track3");
+   if (mag1 || mag2 || mag3)
+   {
+      j_t m = j_store_array(j, "mag");
+      j_append_string(m, mag1);
+      j_append_string(m, mag2);
+      j_append_string(m, mag3);
+   }
+   j_t p = j_store_array(j, "print");
+   unsigned char *panel = malloc(cols * rows);
+   for (int side = 0; side < 2; side++)
+   {
+      j_t s = j_append_object(p);
+      void add(int layer) {     // base64 uses alloca so make a function, why not
+         int f = open(tmp[side][layer], O_RDONLY);
+         if (f < 0)
+            status("*Cannot open png tmp file");
+         struct stat st;
+         if (fstat(f, &st) < 0)
+            status("*Cannot stat png tmp file");
+         size_t length = st.st_size;
+         void *addr = mmap(NULL, length, PROT_READ, MAP_PRIVATE, f, 0);
+         if (addr == MAP_FAILED)
+            status("*Cannot map png tmp file");
+         const char *tag[] = { "C", "K", "U" };
+         j_store_stringf(s, tag[layer], "data:image/png;base64,%s", j_base64(length, addr));
+         munmap(addr, length);
+         close(f);
+      }
+      for (int layer = 0; layer < 3; layer++)
+         if (tmp[side][layer])
+            add(layer);
+   }
+   free(panel);
+   // Cleanup
+   unlink(tmpsvg);
+   free(tmpsvg);
+   for (int side = 0; side < 2; side++)
+      for (int layer = 0; layer < 3; layer++)
+      {
+         unlink(tmp[side][layer]);
+         free(tmp[side][layer]);
+      }
+   xml_tree_delete(svg);
+   return j;
+}
+
 #ifndef LIB
+void status(const char *status)
+{                               // Report status (if start * then error)
+   if (jsstatus)
+   {
+      printf("<script>document.getElementById('%s').innerHTML='%s';</script>", jsstatus, status);
+      fflush(stdout);
+   }
+   if (*status == '*')
+      errx(1, "%s", status + 1);
+}
+
 int main(int argc, const char *argv[])
 {
    int count = 0;
@@ -128,16 +270,6 @@ int main(int argc, const char *argv[])
       }
       poptFreeContext(optCon);
    }
-   void status(const char *status) {    // Report status (if start * then error)
-      if (jsstatus)
-      {
-         printf("<script>document.getElementById('%s').innerHTML='%s';</script>", jsstatus, status);
-         fflush(stdout);
-      }
-      if (*status == '*')
-         errx(1, "%s", status + 1);
-   }
-
    if (output && !freopen(output, "w", stdout))
       status("*Cannot open output");
    if (input && !freopen(input, "r", stdin))
@@ -169,142 +301,9 @@ int main(int argc, const char *argv[])
          status("*Cols mismatch");
    }
 
-   j_t compose(void) {          // Convert SVG to png
-      status("Compose");
-      if (dpi < 0)
-         dpi = 300;
-      if (rows < 0)
-         rows = 664 * dpi / 300;
-      if (cols < 0)
-         cols = 1036 * dpi / 300;
-      char *tmpsvg = strdup("/tmp/cardXXXXXX.svg");
-      {
-         int f = mkstemps(tmpsvg, 4);
-         if (f < 0)
-            status("*Cannot make svg tmp file");
-         FILE *o = fdopen(f, "w");
-         xml_write(o, svg);
-         fclose(o);
-      }
-      const char layertag[] = "CKU";
-      char *tmp[2][3] = { };
-      pid_t pid[2][3] = { };
-      for (int side = 0; side < 2; side++)
-         for (int layer = 0; layer < 3; layer++)
-         {
-            xml_t find(xml_t x) {
-               const char *v = xml_get(x, "@id");
-               if (v && v[0] == layertag[layer] && v[1] == '1' + side && !v[2])
-                  return x;
-               xml_t e = NULL,
-                   q;
-               while ((e = xml_element_next(x, e)))
-                  if ((q = find(e)))
-                     return q;
-               return NULL;
-            }
-            if (!find(svg))
-               continue;
-            tmp[side][layer] = strdup("/tmp/cardXX-XXXXXX.png");
-            tmp[side][layer][9] = layertag[layer];
-            tmp[side][layer][10] = '1' + side;
-            int f = mkstemps(tmp[side][layer], 4);
-            if (f < 0)
-               status("*Cannot make PNG tmp file");
-            close(f);
-            char id[3] = { layertag[layer], '1' + side };
-            if (!(pid[side][layer] = fork()))
-            {
-               char *args[100];
-               int a = 0,
-                   i;
-               args[a++] = "inkscape";
-               args[a++] = "--without-gui";
-               args[a++] = "--export-area-page";
-               args[a++] = "--export-id-only";
-               if (asprintf(&args[a++], "--export-png=%s", tmp[side][layer]) < 0)
-                  status("*malloc");
-               if (asprintf(&args[a++], "--export-dpi=%d", dpi) < 0)
-                  status("*malloc");
-               if (asprintf(&args[a++], "--export-id=%s", id) < 0)
-                  status("*malloc");
-               args[a++] = tmpsvg;
-               args[a++] = NULL;
-               if (debug)
-                  for (i = 0; i < a; i++)
-                     fprintf(stderr, "%s%s", i ? " " : "", args[i] ? : "\n");
-               int n = open("/dev/null", 0);
-               dup2(n, 1);
-               dup2(n, 2);
-               close(n);
-               execv("/usr/bin/inkscape", (char *const *) args);
-               err(1, "Failed to run inkscape");
-            }
-         }
-      for (int side = 0; side < 2; side++)
-         for (int layer = 0; layer < 3; layer++)
-            if (tmp[side][layer])
-            {
-               int pstatus = 0;
-               waitpid(pid[side][layer], &pstatus, 0);
-               if (!WIFEXITED(pstatus) || WEXITSTATUS(pstatus))
-                  status("*SVG conversion fail");
-            }
-      j_t j = j_create();
-      char *mag1 = xml_get(svg, "@track1");
-      char *mag2 = xml_get(svg, "@track2");
-      char *mag3 = xml_get(svg, "@track3");
-      if (mag1 || mag2 || mag3)
-      {
-         j_t m = j_store_array(j, "mag");
-         j_append_string(m, mag1);
-         j_append_string(m, mag2);
-         j_append_string(m, mag3);
-      }
-      j_t p = j_store_array(j, "print");
-      unsigned char *panel = malloc(cols * rows);
-      for (int side = 0; side < 2; side++)
-      {
-         j_t s = j_append_object(p);
-         void add(int layer) {  // base64 uses alloca so make a function, why not
-            int f = open(tmp[side][layer], O_RDONLY);
-            if (f < 0)
-               status("*Cannot open png tmp file");
-            struct stat st;
-            if (fstat(f, &st) < 0)
-               status("*Cannot stat png tmp file");
-            size_t length = st.st_size;
-            void *addr = mmap(NULL, length, PROT_READ, MAP_PRIVATE, f, 0);
-            if (addr == MAP_FAILED)
-               status("*Cannot map png tmp file");
-            const char *tag[] = { "C", "K", "U" };
-            j_store_stringf(s, tag[layer], "data:image/png;base64,%s", j_base64(length, addr));
-            munmap(addr, length);
-            close(f);
-         }
-         for (int layer = 0; layer < 3; layer++)
-            if (tmp[side][layer])
-               add(layer);
-      }
-      free(panel);
-      // Cleanup
-      if (!debug)
-         unlink(tmpsvg);
-      free(tmpsvg);
-      for (int side = 0; side < 2; side++)
-         for (int layer = 0; layer < 3; layer++)
-         {
-            if (!debug)
-               unlink(tmp[side][layer]);
-            free(tmp[side][layer]);
-         }
-      xml_tree_delete(svg);
-      return j;
-   }
-
    if (preview)
    {
-      j_t j = compose();
+      j_t j = xid_compose(svg, dpi, rows, cols, status);
       j_t p = j_find(j, "print");
       for (j_t s = j_first(p); s; s = j_next(s))
          for (j_t l = j_first(s); l; l = j_next(l))
@@ -404,7 +403,7 @@ int main(int argc, const char *argv[])
          }
          if (j_find(i, "id"))
          {                      // Send print
-            j_t j = compose();
+            j_t j = xid_compose(svg, dpi, rows, cols, status);
             j_err(j_write_func(j, ss_write_func, ss));
             j_delete(&j);
             status("Printing");
--- xidsvg.h
+++ xidsvg.h
@@ -3,3 +3,7 @@
 // TODO Connect to print server
 // TODO Convert SVG to JSON
 // TODO Disconnect
+
+
+typedef void xid_status_t(const char *);        // Status reporting function
+j_t xid_compose(xml_t, int dpi, int rows, int cols, xid_status_t *);
