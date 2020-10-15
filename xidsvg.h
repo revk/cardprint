aab57fcb22c68f48bb3e27f0ebfec32f6dc4f711
Adrian Kennard <a@k.gg>
2020-10-15 09:56:20 +0100

    typo

--- xidsvg.h
+++ xidsvg.h
@@ -1,6 +1,6 @@
 // XID SVG library
 //
 typedef void xid_status_t(const char *);        // Status reporting function
-void xid_set_stratus(xid_status_t *);
+void xid_set_status(xid_status_t *);
 j_t xid_compose(xml_t, int dpi, int rows, int cols);
 const char *xid_connect(const char *sizeserver, const char *xidport, const char *keyfile, const char *certfile, j_stream_t * jin);
