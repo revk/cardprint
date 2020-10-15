a696d421d2b359ba261bc07b4627dcadcc087276
Adrian Kennard <a@k.gg>
2020-10-15 10:11:48 +0100

    Typo

--- xidsvg.h
+++ xidsvg.h
@@ -4,5 +4,5 @@ typedef void xid_status_t(const char *);        // Status reporting function
 void xid_set_status(xid_status_t *);
 j_t xid_compose(xml_t, int dpi, int rows, int cols);
 const char *xid_connect(const char *xidserver, const char *keyfile, const char *certfile, j_stream_t * jin);
-ssize_t xidwrite_func(void *arg, void *buf, size_t len);
+ssize_t xid_write_func(void *arg, void *buf, size_t len);
 
